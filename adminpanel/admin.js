419b906dba0d0fee621b5c66667c245e2804ae5b
zchen41 <zchen41@illinois.edu>
2014-12-03 13:52:44 -0600

    updated webui

--- adminpanel/admin.js
+++ adminpanel/admin.js
@@ -1,3 +1,25 @@
 $(function(){
+	$(".selected_content").fadeIn(200);
 
+	$(".item").click(function(){
+		$(".selected_group").removeClass("selected_group");
+		//var curr_class = $(this).attr("class");
+		//console.log(curr_class);
+		//$(this).attr("class", curr_class + " selected_group");
+		$(this).parent().addClass("selected_group");
+		var target = $(this).attr("data-target");
+		var target_obj =$("#"+target);
+		var curr = $(".selected_content");
+		curr.removeClass("selected_content");
+		target_obj.addClass("selected_content");
+		//if(curr.attr("id")!=target) {
+			/*curr.fadeOut(200, function(){
+				target_obj.fadeIn(200);
+			});*/
+		//}
+	});
+	
+	/*$(".collapsible").click(function(){
+		$(this).toggleClass("collapsed").toggleClass("shown");
+	});*/
 });
